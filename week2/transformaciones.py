44d21dc5fc12b6e3f526e6cc891c002485c74012
Dara <menesesdara@gmail.com>
2021-10-10 21:37:11 +0000

    intento 1

--- week2/transformaciones.py
+++
@@ -1,9 +0,0 @@
-from math import sin, cos
-def esfericas(r,t,p):
-    x= r*sin(t)*cos(p)
-    y= r*sin(t)*sin(p)
-    z= r*cos(t)
-    return x, y, z
-
-x, y, p = esfericas(1,5,1.1071487177940904)
-print("La coordenada esférica es (",r,",",theta,",",phi,")")
